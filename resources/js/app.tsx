bfaf942d9594638c6b78c0ccdec8d1d1d7f6bb16
Joshua Gretton <jb.gretton@googlemail.com>
2026-06-07 14:17:42 +0100

    fix(darkmode): removed initialise theme
    removing this stop users from using dark mode. I will re add this in the
    future once I work on a better design for the application

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,10 +1,9 @@
-import { createInertiaApp } from '@inertiajs/react';
 import { Toaster } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { initializeTheme } from '@/hooks/use-appearance';
 import AppLayout from '@/layouts/app-layout';
 import AuthLayout from '@/layouts/auth-layout';
 import SettingsLayout from '@/layouts/settings/layout';
+import { createInertiaApp } from '@inertiajs/react';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -37,4 +36,4 @@ createInertiaApp({
 });
 
 // This will set light / dark mode on load...
-initializeTheme();
+// initializeTheme();
